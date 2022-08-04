8d61c8cb0772dcfad52ff07ca8827e3f7673d2e4
phildong <zhe.dong@icahn.mssm.edu>
2022-08-04 15:34:05 -0400

    update plots for presentation

--- 00.generate_data.py
+++ 00.generate_data.py
@@ -7,6 +7,7 @@ import plotly.express as px
 from routine.data_generation import generate_data
 
 FIG_PATH = "./figs/data"
+PARAM_FONT_SZ = {"font_size": 16, "title_font_size": 24, "legend_title_font_size": 24}
 os.makedirs(FIG_PATH, exist_ok=True)
 
 #%% generate data with hidden feature depending on cohort
@@ -18,7 +19,6 @@ obs_df, user_df, camp_df = generate_data(
     num_cohort=10,
     cohort_variances=np.linspace(0.01, 0.1, 10),
     fh_cohort=True,
-    response_sig_a=10,
 )
 # plot user features
 fig_user = px.scatter_3d(
@@ -30,14 +30,17 @@ fig_user = px.scatter_3d(
 )
 fig_user.update_traces(marker_size=3)
 fig_user.update_layout(
-    legend={"itemsizing": "constant"}, title="Hidden features dependent on cohorts"
+    legend={"itemsizing": "constant"},
+    title="Hidden features dependent on cohorts",
+    **PARAM_FONT_SZ,
 )
 fig_user.write_html(os.path.join(FIG_PATH, "user_fh_cohort.html"))
 # plot response cdf
-fig_resp = px.histogram(
-    obs_df, x="response", nbins=500, histnorm="probability", cumulative=True
-)
-fig_resp.write_image(os.path.join(FIG_PATH, "resp_fh_cohort.svg"))
+resp_df = obs_df.groupby(["cohort", "response"]).size().rename("freq").reset_index()
+fig_resp = px.bar(resp_df, x="response", y="freq", facet_col="cohort", facet_col_wrap=5)
+fig_resp.add_hline(100000 * 100 / 20, line_dash="dot", line_color="gray")
+fig_resp.update_layout(title="Hidden features dependent on cohorts", **PARAM_FONT_SZ)
+fig_resp.write_html(os.path.join(FIG_PATH, "resp_fh_cohort.html"))
 
 #%% generate data with hidden feature independnt of cohort
 # generate data
@@ -48,7 +51,6 @@ obs_df, user_df, camp_df = generate_data(
     num_cohort=10,
     cohort_variances=0.05,
     fh_cohort=False,
-    response_sig_a=10,
 )
 # plot user features
 fig_user = px.scatter_3d(
@@ -60,21 +62,19 @@ fig_user = px.scatter_3d(
 )
 fig_user.update_traces(marker_size=3)
 fig_user.update_layout(
-    legend={"itemsizing": "constant"}, title="Hidden features independent of cohorts"
+    legend={"itemsizing": "constant"},
+    title="Hidden features independent of cohorts",
+    **PARAM_FONT_SZ,
 )
 fig_user.write_html(os.path.join(FIG_PATH, "user_fh_ind.html"))
 # plot campaign features
 fig_camp = px.scatter_3d(camp_df, x="camp_f0", y="camp_f1", z="camp_fh")
 fig_camp.update_traces(marker_size=3)
-fig_camp.update_layout(legend={"itemsizing": "constant"})
+fig_camp.update_layout(legend={"itemsizing": "constant"}, **PARAM_FONT_SZ)
 fig_camp.write_html(os.path.join(FIG_PATH, "camp.html"))
 # plot response cdf
-fig_resp = px.histogram(
-    obs_df, x="response", nbins=500, histnorm="probability", cumulative=True
-)
-fig_resp.write_image(os.path.join(FIG_PATH, "resp_fh_ind.svg"))
-# plot iprod cdf
-fig_resp = px.histogram(
-    obs_df, x="iprod", nbins=500, histnorm="probability", cumulative=True
-)
-fig_resp.write_image(os.path.join(FIG_PATH, "iprod.svg"))
+resp_df = obs_df.groupby(["cohort", "response"]).size().rename("freq").reset_index()
+fig_resp = px.bar(resp_df, x="response", y="freq", facet_col="cohort", facet_col_wrap=5)
+fig_resp.add_hline(100000 * 100 / 20, line_dash="dot", line_color="gray")
+fig_resp.update_layout(title="Hidden features independent of cohorts", **PARAM_FONT_SZ)
+fig_resp.write_html(os.path.join(FIG_PATH, "resp_fh_ind.html"))
