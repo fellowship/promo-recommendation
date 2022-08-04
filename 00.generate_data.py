a92702fb98924e423ae3e7229e229694a3f28a0f
phildong <zhe.dong@icahn.mssm.edu>
2022-08-04 12:45:34 -0400

    generate more example data figures for presentation

--- 00.generate_data.py
+++ 00.generate_data.py
@@ -9,7 +9,8 @@ from routine.data_generation import generate_data
 FIG_PATH = "./figs/data"
 os.makedirs(FIG_PATH, exist_ok=True)
 
-#%% generate data
+#%% generate data with hidden feature depending on cohort
+# generate data
 obs_df, user_df, camp_df = generate_data(
     num_users=1000,
     num_campaigns=100,
@@ -19,8 +20,7 @@ obs_df, user_df, camp_df = generate_data(
     fh_cohort=True,
     response_sig_a=10,
 )
-
-#%% plot user features
+# plot user features
 fig_user = px.scatter_3d(
     user_df.astype({"cohort": str}),
     x="user_f0",
@@ -28,17 +28,42 @@ fig_user = px.scatter_3d(
     z="user_fh",
     color="cohort",
 )
-fig_user.update_traces(marker_size=2)
-fig_user.update_layout(legend={"itemsizing": "constant"})
-fig_user.write_html(os.path.join(FIG_PATH, "user.html"))
+fig_user.update_traces(marker_size=3)
+fig_user.update_layout(
+    legend={"itemsizing": "constant"}, title="Hidden features dependent on cohorts"
+)
+fig_user.write_html(os.path.join(FIG_PATH, "user_fh_cohort.html"))
 
-#%% plot campaign features
+#%% generate data with hidden feature independnt of cohort
+# generate data
+obs_df, user_df, camp_df = generate_data(
+    num_users=1000,
+    num_campaigns=100,
+    samples_per_campaign=100000,
+    num_cohort=10,
+    cohort_variances=0.05,
+    fh_cohort=False,
+    response_sig_a=10,
+)
+# plot user features
+fig_user = px.scatter_3d(
+    user_df.astype({"cohort": str}),
+    x="user_f0",
+    y="user_f1",
+    z="user_fh",
+    color="cohort",
+)
+fig_user.update_traces(marker_size=3)
+fig_user.update_layout(
+    legend={"itemsizing": "constant"}, title="Hidden features independent of cohorts"
+)
+fig_user.write_html(os.path.join(FIG_PATH, "user_fh_ind.html"))
+# plot campaign features
 fig_camp = px.scatter_3d(camp_df, x="camp_f0", y="camp_f1", z="camp_fh")
-fig_camp.update_traces(marker_size=2)
+fig_camp.update_traces(marker_size=3)
 fig_camp.update_layout(legend={"itemsizing": "constant"})
 fig_camp.write_html(os.path.join(FIG_PATH, "camp.html"))
-
-#%% plot response cdf
+# plot response cdf
 fig_resp = px.histogram(
     obs_df, x="response", nbins=500, histnorm="probability", cumulative=True
 )
