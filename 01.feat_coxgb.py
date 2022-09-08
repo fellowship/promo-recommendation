62f2066329d698820b01e68b201c3009b607cd35
phildong <zhe.dong@icahn.mssm.edu>
2022-09-08 12:18:08 -0400

    add analysis on new users or new campaigns

--- 01.feat_coxgb.py
+++ 01.feat_coxgb.py
@@ -11,6 +11,7 @@ from tqdm.auto import tqdm
 
 from routine.data_generation import generate_data
 from routine.models import CohortXGB
+from routine.training import cv_by_id
 import plotly.express as px
 
 PARAM_DATA = {
@@ -56,6 +57,8 @@ PARAM_MAP = {
 }
 PARAM_NTRAIN = 20
 PARAM_FONT_SZ = {"font_size": 16, "title_font_size": 24, "legend_title_font_size": 24}
+PARAM_CV = 5
+PARAM_SPLT_BY = ["camp_id", "user_id"]
 OUT_RESULT_PATH = "./intermediate/feat_coxgb"
 FIG_PATH = "./figs/feat_coxgb"
 os.makedirs(OUT_RESULT_PATH, exist_ok=True)
@@ -63,57 +66,39 @@ os.makedirs(FIG_PATH, exist_ok=True)
 
 #%% training
 result_ls = []
-for cvar, pkey, itrain in tqdm(
-    list(itt.product(PARAM_VAR, PARAM_MAP.keys(), range(PARAM_NTRAIN)))
+for cvar, pkey, splt_by, itrain in tqdm(
+    list(itt.product(PARAM_VAR, PARAM_MAP.keys(), PARAM_SPLT_BY, range(PARAM_NTRAIN)))
 ):
     cohort_var = np.array([cvar, cvar, 0.1])
-    data_train, user_df, camp_df = generate_data(
-        cohort_variances=cohort_var, **PARAM_DATA
-    )
-    data_user, _, _ = generate_data(
-        cohort_variances=cohort_var, user_df=user_df, **PARAM_DATA
-    )
-    data_test, _, _ = generate_data(cohort_variances=cohort_var, **PARAM_DATA)
+    data, _, _ = generate_data(cohort_variances=cohort_var, **PARAM_DATA)
     cur_param = PARAM_MAP[pkey]
-    model = CohortXGB(n_cohort=PARAM_DATA["num_cohort"], **cur_param, **PARAM_XGB)
-    model.fit(data_train)
+    scores = []
+    for data_train, data_test in cv_by_id(data, PARAM_CV, splt_by):
+        model = CohortXGB(n_cohort=PARAM_DATA["num_cohort"], **cur_param, **PARAM_XGB)
+        model.fit(data_train)
+        scores.append(model.score(data_test))
     score = pd.DataFrame(
-        [
-            {
-                "cohort_variance": cvar,
-                "feats": pkey,
-                "itrain": itrain,
-                "score_test": model.score(data_test),
-                "score_user": model.score(data_user),
-            }
-        ]
+        {
+            "cohort_variance": cvar,
+            "feats": pkey,
+            "itrain": itrain,
+            "split_by": splt_by,
+            "cv": np.arange(PARAM_CV),
+            "scores": np.array(scores),
+        }
     )
     result_ls.append(score)
 result = pd.concat(result_ls, ignore_index=True)
 result.to_csv(os.path.join(OUT_RESULT_PATH, "result.csv"), index=False)
 
 #%% plot result
-data_map = {
-    "score_train": "Training data",
-    "score_valid": "Seen users, Seen campaigns",
-    "score_user": "Seen users, New campaigns",
-    "score_test": "New users, New campaigns",
-}
-result = (
-    pd.read_csv(os.path.join(OUT_RESULT_PATH, "result.csv"))
-    .melt(
-        id_vars=["cohort_variance", "feats", "itrain"],
-        var_name="data_type",
-        value_name="score",
-    )
-    .replace({"data_type": data_map})
-)
+result = pd.read_csv(os.path.join(OUT_RESULT_PATH, "result.csv"))
 fig = px.box(
     result,
     x="cohort_variance",
-    y="score",
+    y="scores",
     color="feats",
-    facet_col="data_type",
-    labels={"score": "CV Score", "cohort_variance": "Cohort Variance"},
+    facet_row="split_by",
+    labels={"scores": "CV Score", "cohort_variance": "Cohort Variance"},
 )
 fig.write_html(os.path.join(FIG_PATH, "scores.html"))
