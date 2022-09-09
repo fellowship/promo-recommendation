80cb88a09c98422de5e35309e36442eb3e2fed53
phildong <zhe.dong@icahn.mssm.edu>
2022-09-08 22:26:41 -0400

    add case study

--- 01.feat_coxgb.py
+++ 01.feat_coxgb.py
@@ -14,6 +14,8 @@ from tqdm.auto import tqdm
 from routine.data_generation import generate_data
 from routine.models import CohortXGB
 from routine.training import cv_by_id
+from routine.plotting import scatter_3d
+from plotly.express.colors import qualitative
 
 PARAM_DATA = {
     "num_users": 1000,
@@ -110,3 +112,130 @@ for yvar in ["scores", "cohort_mi"]:
         labels={"scores": "CV Score", "cohort_variance": "Visible Feature Variance"},
     )
     fig.write_html(os.path.join(FIG_PATH, "{}.html".format(yvar)))
+
+#%% case study of cohort accuracy
+def plot_users(df, cmap, z="user_fh", col="cohort"):
+    df[col + "_col"] = df[col].map(cmap)
+    fig = scatter_3d(
+        df.astype({col: str}),
+        x="user_f0",
+        y="user_f1",
+        z=z,
+        mode="markers",
+        marker={"color": col + "_col"},
+        legend_dim=col,
+        facet_col="visible_variance",
+        facet_row=None,
+    )
+    fig.update_traces(marker_size=3)
+    return fig
+
+
+vis_var = {"low": 0.1, "high": 0.6}
+var_fh = 0.1
+means = np.array([[0.5, 0.5, 0.5], [-0.5, -0.5, -0.5]])
+param_data = {
+    "num_users": 1000,
+    "num_campaigns": 1,
+    "samples_per_campaign": 2000,
+    "num_cohort": 2,
+    "fh_cohort": True,
+    "even_cohort": True,
+    "response_sig_a": 10,
+    "cross_weight": None,
+    "magnify_hf": 1,
+}
+param_model = {
+    "n_cohort": 2,
+    "feats": ["cohort", "user_f0", "user_f1", "camp_f0", "camp_f1", "camp_fh"],
+    "cohort_feats": ["user_f0", "user_f1"],
+    "max_depth": 5,
+    "learning_rate": 1,
+    "objective": "binary:logistic",
+    "eval_metric": "logloss",
+    "use_label_encoder": False,
+}
+camp_df = pd.DataFrame(
+    [
+        {
+            "camp_id": 0,
+            "freq": param_data["samples_per_campaign"],
+            "camp_f0": 1,
+            "camp_f1": 1,
+            "camp_fh": 1,
+        }
+    ]
+)
+ax_title_feat = {
+    "xaxis_title": "Feature 1",
+    "yaxis_title": "Feature 2",
+    "zaxis_title": "Hidden Feature",
+}
+ax_title_resp = {
+    "xaxis_title": "Feature 1",
+    "yaxis_title": "Feature 2",
+    "zaxis_title": "Response",
+}
+cmap_real = {0: qualitative.Plotly[0], 1: qualitative.Plotly[1]}
+cmap_prd = {0: qualitative.Plotly[2], 1: qualitative.Plotly[3]}
+data_df = []
+user_df = []
+for var_lev, cur_var in vis_var.items():
+    cur_data, cur_user, _ = generate_data(
+        cohort_means=means,
+        cohort_variances=np.array([cur_var, cur_var, var_fh]),
+        camp_df=camp_df,
+        **param_data,
+    )
+    model = CohortXGB(use_cohort_resp=False, **param_model)
+    model.fit(cur_data)
+    cur_data["cohort_cluster"] = model.predict_cohort(cur_data)
+    model = CohortXGB(use_cohort_resp=True, **param_model)
+    model.fit(cur_data)
+    cur_data["cohort_resp"] = model.predict_cohort(cur_data)
+    cur_data["visible_variance"] = var_lev
+    cur_user["visible_variance"] = var_lev
+    data_df.append(cur_data)
+    user_df.append(cur_user)
+data_df = pd.concat(data_df, ignore_index=True)
+user_df = pd.concat(user_df, ignore_index=True)
+# plot user features colored by real cohort
+fig_user = plot_users(user_df, cmap_real)
+fig_user.update_layout(
+    legend={"title": "Cohort", "itemsizing": "constant"},
+    title="Real Cohorts",
+    scene=ax_title_feat,
+    scene2=ax_title_feat,
+    **PARAM_FONT_SZ,
+)
+fig_user.write_html(os.path.join(FIG_PATH, "user_real_cohort.html"))
+# plot clustering without response
+fig_cluster = plot_users(data_df, cmap_prd, col="cohort_cluster")
+fig_cluster.update_layout(
+    legend={"title": "Predicted Cohort", "itemsizing": "constant"},
+    title="Visible Clustered Cohorts",
+    scene=ax_title_feat,
+    scene2=ax_title_feat,
+    **PARAM_FONT_SZ,
+)
+fig_cluster.write_html(os.path.join(FIG_PATH, "cluster_cohort.html"))
+# plot clustering with response
+fig_resp = plot_users(data_df, cmap_real, z="response", col="cohort")
+fig_resp.update_layout(
+    legend={"title": "Cohort", "itemsizing": "constant"},
+    title="Augmented Space",
+    scene=ax_title_resp,
+    scene2=ax_title_resp,
+    **PARAM_FONT_SZ,
+)
+fig_resp.write_html(os.path.join(FIG_PATH, "resp_cohort.html"))
+# plot clustering with response
+fig_prd = plot_users(data_df, cmap_prd, col="cohort_resp")
+fig_prd.update_layout(
+    legend={"title": "Predicted Cohort", "itemsizing": "constant"},
+    title="Response Clustered Cohorts",
+    scene=ax_title_resp,
+    scene2=ax_title_resp,
+    **PARAM_FONT_SZ,
+)
+fig_prd.write_html(os.path.join(FIG_PATH, "prd_cohort.html"))
